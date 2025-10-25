ff4db32a60e92e35486d35e3a9a03d24382dd40d
sarath chandra reddy <sarathchandraramireddy@gmail.com>
2025-10-25 09:15:58 +0530

    tailwind project done

---
+++ Frontend/tailwind-project/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+        <h1 className="text-red-500 m-10">Hello Javascript</h1>
+        <button className="ml-10 bg-black text-white rounded-md p-2">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
