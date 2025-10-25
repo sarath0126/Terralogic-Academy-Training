c6847bb71640dbe41a844ae5bfd6578d983db821
sarath chandra reddy <sarathchandraramireddy@gmail.com>
2025-10-25 09:24:18 +0530

    Code Updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sarath</h1>
+      <p>Hey Hi This is Sarath</p>
+    </div>
+  );
+}
+
+export default App;
